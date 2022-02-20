79524bf9d3126033187f2d90ac232bcacedbe9c9
Yuvraj-50 <93057232+Yuvraj-50@users.noreply.github.com>
2022-02-20 15:27:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const adiveId = document.getElementById("advice-id");
 const adviceText = document.getElementById("advice-text");
 const btn = document.getElementById("btn");
+const card = document.querySelector("main");
 
 function newAdvice() {
   fetch("https://api.adviceslip.com/advice")
-    .then((res) => res.json())
+    .then((res) => {
+      if (!res.ok) throw new Error(`something went wrong ERROR ${res.status}`);
+      return res.json();
+    })
     .then((data) => {
       adiveId.textContent = `${data.slip.id}`;
       adviceText.textContent = `${data.slip.advice}`;
+    })
+    .catch((err) => {
+      console.error(err.message);
+      card.textContent = `${err.message}`;
     });
 }
 
